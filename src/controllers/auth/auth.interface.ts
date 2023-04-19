fdfd8dda63694f7ff0f23a429a797c28c2646c0e
Berat Azgun <brtazgun@gmail.com>
2023-04-19 13:57:14 +0300

    added new interfaces

--- src/controllers/auth/auth.interface.ts
+++ src/controllers/auth/auth.interface.ts
@@ -5,3 +5,24 @@ export interface ISigninData {
 	email: string
 	password: string
 }
+
+export interface IUpdatePasswordData {
+	currentPassword: string
+	newPassword: string
+	newPasswordConfirm: string
+}
+
+export interface IResetPasswordData {
+	password: string
+	passwordConfirm: string
+}
+
+export interface ISignupData {
+	firstName: string
+	lastName: string
+	email: string
+	phone: string
+	password: string
+	passwordConfirm: string
+	role: string
+}
