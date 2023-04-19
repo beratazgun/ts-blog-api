1a6842c8e810ccb95e5613de61a9c022431a926d
Berat Azgun <brtazgun@gmail.com>
2023-04-19 13:58:04 +0300

    created account.routes.ts

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,10 +1,15 @@
 import express from 'express'
-import { protectRoute, confirmAccount } from '../controllers/auth/auth.controller'
+import {
+	protectRoute,
+	confirmAccount,
+	sendForgotPasswordEmail,
+	resetPassword,
+} from '../controllers/auth/auth.controller'
 
 const router = express.Router()
+router.route('/forgot-password').post(sendForgotPasswordEmail)
+router.route('/ap/reset-password').post(resetPassword)
 
-router.use(protectRoute)
-
-router.route('/verify/confirm-account').post(confirmAccount)
+router.route('/verify/confirm-account').post(protectRoute, confirmAccount)
 
 export default router
